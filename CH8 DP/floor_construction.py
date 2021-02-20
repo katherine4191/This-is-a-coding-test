333d6ebf961893d77e9946c62e7618e2c6255748
Soyoung <ma2214191@gmail.com>
2021-02-20 16:13:48 +0900

    "0220 바닥 공사"

--- CH8 DP/floor_construction.py
+++ CH8 DP/floor_construction.py
@@ -2,6 +2,11 @@
 n = 345
 div_num = 796796
 
+'''
+input:  [760,    765,    378,    345,    4,  521,    741,    640,    464,    530] # n이 들어있는 배열
+output: [536911, 448425, 551139, 336313, 11, 373121, 481185, 496871, 646635, 603879] # n에 해당하는 answer가 들어있는 배열
+'''
+
 table = [0 for _ in range(1001)] # 1 <= n <= 1000, table = [0] * 1001
 table[1], table[2] = 1, 3
 
@@ -10,11 +15,10 @@ for i in range(3, n + 1):
 
 print(table[n] % div_num)
 
+'''testcase 생성하기'''
 import random
 def create_input(min, max, N):
     input_list = random.sample(range(min,max), N)
     print(input_list)
 
 #create_input(1, 1000, 10)
-# input:  [760,    765,    378,    345,    4,  521,    741,    640,    464,    530]
-# output: [536911, 448425, 551139, 336313, 11, 373121, 481185, 496871, 646635, 603879]
