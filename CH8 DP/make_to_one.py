07a40cd545de9108763c21a199266b7dbf7b0922
Soyoung <ma2214191@gmail.com>
2021-02-21 10:51:13 +0900

    "ch8 1로 만들기 수정"

--- CH8 DP/make_to_one.py
+++ CH8 DP/make_to_one.py
@@ -6,9 +6,6 @@ output: 3
 
 input 2: [20730, 16465, 27368, 21803, 29081, 23215, 16419, 12244, 16602, 25045]
 output 2: 10 13 11 13 14 13 11 11 11 10
-
-input 3: [20730, 16465, 27368, 21803, 29081, 23215, 16419, 12244, 16602, 25045]
-output 3: 8 10 9 9 11 11 11 8 11 10 
 '''
 nums = [8000, 6814, 2553, 3918, 7034, 5806, 5471, 3012, 6986, 5347]
 
@@ -24,11 +21,11 @@ def make_to_one(n):
         candidates = [30000,30000,30000,30000]
         candidates[0] = arr[a-1] + 1
         if a % 2 == 0:
-            candidates[1] = arr[int(a/2)] + 1
+            candidates[1] = arr[a//2] + 1
         if a % 3 == 0:
-            candidates[2] = arr[int(a/3)] + 1
+            candidates[2] = arr[a//3] + 1
         if a % 5 == 0:  
-            candidates[3] = arr[int(a/5)] + 1
+            candidates[3] = arr[a//5] + 1
         arr[a] = min(candidates)
 
     print(arr[n], end = ' ')
