82cef45b1d58c37c4913734b5583b42c79409ccb
Soyoung <ma2214191@gmail.com>
2021-02-22 21:25:14 +0900

    "ch9 개선된 다익스트라"

--- CH9 Short path/improved_dijkstra.py
+++ CH9 Short path/improved_dijkstra.py
@@ -15,7 +15,19 @@ for i in range(e):
     graph[a].append((b, cost))
 
 def dijkstra(n_start): 
-    pass
+    global dist
+    que = []
+    heapq.heappush(que, (0, n_start)) # 첫 번째 원소 기준으로 우선순위를 정함
+    dist[n_start] = 0
+    while que:
+        distance, now = heapq.heappop(que)
+        if dist[now] < distance: # 이미 처리되어 최단 거리로 갱신된 경우
+            continue
+        for b, cost in graph[now]:
+            next_cost = distance + cost
+            if next_cost < dist[b]: # 현재 노드를 거쳐서 b로 가는 거리가 짧은 경우
+                dist[b] = next_cost
+                heapq.heappush(que, (dist[b], b))
 
 dijkstra(n_start)
 
