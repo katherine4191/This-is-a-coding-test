f98e5406c139e8cf7898088220a627dcaaed243d
Soyoung <ma2214191@gmail.com>
2021-03-31 17:22:18 +0900

    "create_function 중복 option 추가"

--- function.py
+++ function.py
@@ -1,7 +1,13 @@
 '''testcase 생성하기'''
 import random
 
-def create_input(min, max, N):
-    input_list = random.sample(range(min,max), N)
-    print(N)
-    print(input_list)
+def create_input(min, max, N, overlap = True):
+    if overlap == False:
+        # ramdom한 정수를 중복되지 않게 sampling
+        input_list = random.sample(range(min,max), N)
+    else:
+        # ramdom한 정수를 중복 허용
+        input_list = [random.randint(min,max) for _ in range(N)]
+        # random.suffle(_list) 데이터의 순서를 무작위로 만들어줌
+
+    print(N);    print(input_list)
